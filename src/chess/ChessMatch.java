51b66efa95d6be9e6b8911a3aae96f97170a20b5
Lucas Inveninato <lucasinveninato@gmail.com>
2021-03-15 15:10:58 -0300

    ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -22,8 +22,11 @@ public class ChessMatch {
 		}
 		return mat;
 	}
+	public void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());	
+	}
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(board, Color.BLACK), new Position(0, 1));
+		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+		placeNewPiece('c', 4, new Rook(board, Color.BLACK));
 	}
 }
