800da20bbf8d55aa6e6602ac277be0945a23d062
Lucas Inveninato <lucasinveninato@gmail.com>
2021-03-19 11:03:28 -0300

    Implement possible moves of King

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -60,7 +60,7 @@ public class ChessMatch {
 	private void initialSetup() {
 		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('e', 4, new King(board, Color.WHITE));
         placeNewPiece('b', 1, new Knight(board, Color.WHITE));
         placeNewPiece('g', 1, new Knight(board, Color.WHITE));
         placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
